1702c0890d112e25176479543d74b280d6c36ed6
Yilun Xu <yilun.xu@oath.com>
2018-10-03 16:51:46 -0700

    change to dynamic table cells

--- FlurryRevenueSampleApp_Swift/RevenueTableViewController.swift
+++ FlurryRevenueSampleApp_Swift/RevenueTableViewController.swift
@@ -9,58 +9,60 @@
 import UIKit
 import StoreKit
 
+enum InAppPurchaseItem: String {
+    case consumableItem = "com.yahoo.flurryiap.flurry_test_consumable"
+    case nonConsumableItem = "com.yahoo.flurryiap.flurry_test_nonconsumable"
+    case autoRenewSubscription = "com.yahoo.flurryiap.flurry_test_autorenew"
+    case freeItem = "com.yahoo.flurryiap.flurry_test_subscription_free"
+    case nonRenewSubscription = "com.yahoo.flurryiap.flurry_test_subscription_nonrenew"
+}
+
 class RevenueTableViewController: UITableViewController, SKProductsRequestDelegate, SKPaymentTransactionObserver {
-    var products = [SKProduct]()
+   
+    var verifiedProducts = [SKProduct]()
     let paymentQueue = SKPaymentQueue.default()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let productsSet: Set = [IAPProduct.consumable.rawValue,
-                             IAPProduct.consumable2.rawValue,
-                             IAPProduct.nonConsumable.rawValue,
-                             IAPProduct.autoRenew.rawValue,
-                             IAPProduct.nonRenew.rawValue,
-                             IAPProduct.free.rawValue]
-        let request = SKProductsRequest(productIdentifiers: productsSet)
+        let products: Set = [InAppPurchaseItem.consumableItem.rawValue,
+                             InAppPurchaseItem.nonConsumableItem.rawValue,
+                             InAppPurchaseItem.autoRenewSubscription.rawValue,
+                             InAppPurchaseItem.nonRenewSubscription.rawValue,
+                             InAppPurchaseItem.freeItem.rawValue]
+        
+        // init request
+        let request = SKProductsRequest(productIdentifiers: products)
         request.delegate = self
         request.start()
+        
+        // add self as an observer, be notified if one or more transactons are being updated
         paymentQueue.add(self)
 
     }
     
-    func purchase(product: IAPProduct) {
-        guard let productToPurchase = products.filter({$0.productIdentifier == product.rawValue}).first else {return}
+    // purchase the item
+    func purchase(product: InAppPurchaseItem) {
+        guard let productToPurchase = verifiedProducts.filter({$0.productIdentifier == product.rawValue}).first else {return}
+        // add this payment to the payment queue
         let payment = SKPayment(product: productToPurchase)
         paymentQueue.add(payment)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
-        case 0: self.purchase(product: .consumable)
-        case 1: self.purchase(product: .consumable2)
-        case 2: self.purchase(product: .nonConsumable)
-        case 3: self.purchase(product: .autoRenew)
-        case 4: self.purchase(product: .nonRenew)
-        case 5: self.purchase(product: .free)
-        default:
-            print("restore")
-        }
-    }
-    
+    // MARK: - delegate mehod for SKProductRequestDelegate
+    // Accepts the reponse from app store that contains the request products information
     func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-        self.products = response.products
+        self.verifiedProducts = response.products
         for product in response.products {
-            print("here \(product.localizedTitle)")
+            print("verified product: \(product.localizedTitle)")
         }
+        print(verifiedProducts.count)
+        tableView.reloadData()
     }
     
-    
+    // MARK: - delegate mehod for SKPaymentTransactionObserver
+    // Tell the observer when one or more transactions are being updated
     func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
         for transaction in transactions {
             switch transaction.transactionState {
@@ -90,5 +92,26 @@ class RevenueTableViewController: UITableViewController, SKProductsRequestDelega
         }
     }
     
-
+    // MARK: - table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return verifiedProducts.count
+    }
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "ITEMS"
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let product =  verifiedProducts[indexPath.row]
+        cell.textLabel?.text = product.localizedTitle
+        let numberFormatter = NumberFormatter()
+        let locale = product.priceLocale
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = locale
+        cell.detailTextLabel?.text = numberFormatter.string(from: product.price)
+        return cell
+    }
+    
 }
